734c068aeaedf1e9cf1c5b477ab1e5fb057bd765
Flor C <florenciabelenc94@gmail.com>
2021-10-20 14:27:02 -0600

    Added timeblock HTML, started changing block colors according to current local time

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 console.log(this);
 console.log(moment());
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
 
+// Header date 
 var headerDateEl = moment().format("dddd LL");
 console.log(headerDateEl);
 $("#currentDay").append(headerDateEl);
+
+// Changing timeblock colors according to time of day
+var now = moment().format('H');
+console.log(now);
+if (now < 9){
+    $("#9amTask").addClass("future");
+    } else if (now > 9){
+        $("#9amTask").addClass("past");
+    }else if (now = 9){
+        $("#9amTask").addClass("present");
+    }
+
+
+
+
+// onclick + local storage for tasks
