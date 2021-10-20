6611c3cb4d948225e9d9535cc79a4dc19f4fb7ba
Flor C <florenciabelenc94@gmail.com>
2021-10-20 16:29:49 -0600

    finished changing block colors according to current local time

--- assets/script.js
+++ assets/script.js
@@ -16,8 +16,61 @@ if (now < 9){
     }else if (now = 9){
         $("#9amTask").addClass("present");
     }
-
-
-
-
+if (now < 10){
+    $("#10amTask").addClass("future");
+    } else if (now > 10){
+        $("#10amTask").addClass("past");
+    }else if (now = 10){
+        $("#10amTask").addClass("present");
+    }
+if (now < 11){
+    $("#11amTask").addClass("future");
+    } else if (now > 11){
+        $("#11amTask").addClass("past");
+    }else if (now = 11){
+        $("#11amTask").addClass("present");
+    }
+if (now < 12){
+    $("#12pmTask").addClass("future");
+    } else if (now > 12){
+        $("#12pmTask").addClass("past");
+    }else if (now = 12){
+        $("#12pmTask").addClass("present");
+    }
+if (now < 13){
+    $("#1pmTask").addClass("future");
+    } else if (now > 13){
+        $("#1pmTask").addClass("past");
+    }else if (now = 13){
+        $("#1pmTask").addClass("present");
+    }
+if (now < 14){
+    $("#2pmTask").addClass("future");
+    } else if (now > 14){
+        $("#2pmTask").addClass("past");
+    }else if (now = 14){
+        $("#2pmTask").addClass("present");
+    }
+if (now < 15){
+    $("#3pmTask").addClass("future");
+    } else if (now > 15){
+        $("#3pmTask").addClass("past");
+    }else if (now = 15){
+        $("#3pmTask").addClass("present");
+    }
+if (now < 16){
+    $("#4pmTask").addClass("future");
+    } else if (now > 16){
+        $("#4pmTask").addClass("past");
+    }else if (now = 16){
+        $("#4pmTask").addClass("present");
+    }
+if (now < 17){
+    $("#5pmTask").addClass("future");
+    } else if (now > 17){
+        $("#5pmTask").addClass("past");
+    }else if (now = 17){
+        $("#5pmTask").addClass("present");
+    }
+    
 // onclick + local storage for tasks
